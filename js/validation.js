8f93684f90c41bdb29ec3f9423514fe281ffdd5f
nana kogawa <kogawanana@gmail.com>
2015-10-18 18:47:50 -0700

    #1 added form validation with angular

---
+++ js/validation.js
@@ -0,0 +1,16 @@
+ var app = angular.module('myApp', []);
+
+ app.controller('FormValController', ['$scope', function($scope) {
+
+  $scope.master = {};
+
+  $scope.update = function(user) {
+   $scope.master = angular.copy(user);
+  };
+
+  $scope.reset = function() {
+   $scope.user = angular.copy($scope.master);
+  };
+
+  $scope.reset();
+ }]);

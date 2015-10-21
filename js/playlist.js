a11d044db4f0d721884f645ad5cfa811e49d0c52
nana kogawa <kogawanana@gmail.com>
2015-10-21 10:38:02 -0700

    Taking the input value for the music search

--- js/playlist.js
+++ js/playlist.js
@@ -1,22 +1,30 @@
  function searchData (event) {
   event.preventDefault();
   var submitRequest = new XMLHttpRequest();
-  //var term = event.target.textContent;
-  submitRequest.open('GET','/spotify/data',true);
+  submitRequest.open('GET','/spotify/data', true);
   submitRequest.setRequestHeader('content-type', 'application/json');
-  submitRequest.send();
+  submitRequest.send(formSubmit());
   submitRequest.addEventListener('load',function () {
 
    if((submitRequest.status === 200) || (submitRequest.status === 304)) {
 
-    var data = JSON.parse(submitRequest.responseText);
+    var searchResults = JSON.parse(submitRequest.responseText);
 
-    for(var i = 0; i < 5; i++) {
-     console.log(data);
+    for(var i = 0; i < 1; i++) {
+     console.log(searchResults);
     }
    }
   });
  }
+
+ function formSubmit() {
+  var form = document.getElementById('search-form');
+  var formData = new Object();
+  formData.searchInput = document.getElementById('searchInput').value;
+  console.log(formData);
+  return JSON.stringify(formData);
+ }
+
 
  var searchButton = document.getElementById('search-btn');
  searchButton.addEventListener('click', searchData, false);

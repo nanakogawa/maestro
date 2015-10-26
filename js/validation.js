6412685625f382afbdaa9cfb4d2911eb1339e500
nana kogawa <kogawanana@gmail.com>
2015-10-26 13:46:09 -0700

    #2 Configured audio

--- js/validation.js
+++ js/validation.js
@@ -8,6 +8,15 @@
   };
  }]);
 
+ app.config( function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+   // Allow same origin resource loads.
+   'self',
+   // Allow loading from our assets domain.  Notice the difference between * and **.
+   'https://p.scdn.co/mp3-preview/**'
+  ]);
+ });
+
  app.controller('PlaylistController', ['$scope', '$http', function($scope, $http) {
 
   $scope.getPlaylist = function() {

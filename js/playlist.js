64ac3bc29320848798bbb33b58e203264e26874d
Nana Kogawa <kogawanana@gmail.com>
2015-10-27 12:14:57 -0700

    Delete playlist.js

--- js/playlist.js
+++
@@ -1,9 +0,0 @@
- function clearSearchInfo() {
-  var info = document.getElementById('search-info');
-  info.style.display = 'none';
-  var result = document.getElementById('result');
-  result.style.display = 'block';
- }
-
- var searchButton = document.getElementById('search-btn');
- searchButton.addEventListener('click', clearSearchInfo, false);
